1d7dbe5c8ed32190df05f93e30401f693c32bfba
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2018-12-22 21:33:11 +0300

    init

---
+++ README.md
@@ -0,0 +1,1 @@
+[![CircleCI](https://circleci.com/gh/telepenin/electromarat.svg?style=svg)](https://circleci.com/gh/telepenin/electromarat)
---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -exo pipefail
+
+# build
+HUGO_ENV=production hugo -v -d public -s src
+
+# navigate into the build output directory
+cd src/public
+
+# if you are deploying to a custom domain
+echo 'dev.electromarat.ru' > CNAME
+
+git config --global user.email "telepenin.nikolay@gmail.com"
+git config --global user.name "circle-ci"
+
+git init
+git add -A
+git commit -m 'deploy [skip ci]'
+
+git push -f git@github.com:telepenin/electromarat.git master:gh-pages
+
+
+cd -
