ecd1eb74cac6b8596509c77e2b75b72fec16d1bf
Nikolay Telepenin <ntelepenin@cloudlinux.com>
2018-12-23 20:26:22 +0300

    One more try

--- deploy.sh
+++ deploy.sh
@@ -23,6 +23,7 @@ git push -f git@github.com:telepenin/electromarat.git master:gh-pages
 # push onto separate repo
 echo 'dev.elektromarat.ru' > CNAME
 git commit -am 'deploy [skip ci]'
+git remote set origin git@github.com:telepenin/elektromarat.git
 git push -f git@github.com:telepenin/elektromarat.git master:gh-pages
 
 cd -
